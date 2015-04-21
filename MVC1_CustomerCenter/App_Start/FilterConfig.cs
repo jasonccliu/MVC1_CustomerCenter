76fc483e080a335d578b999486466c60f4aceb0e
jasonccliu <jason.auo@gmail.com>
2015-04-21 16:39:04 +0800

    initial version

---
+++ MVC1_CustomerCenter/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVC1_CustomerCenter
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ MVC1_CustomerCenter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC1_CustomerCenter.Startup))]
+namespace MVC1_CustomerCenter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
